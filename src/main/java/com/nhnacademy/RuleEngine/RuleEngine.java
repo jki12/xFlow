98b28a69f797584639214c1329c30a472eb01a7a
AoiTuNa <bestspark123@naver.com>
2023-12-14 14:45:26 +0900

    feat/기본 단위 로직 구현 완료

--- src/main/java/com/nhnacademy/RuleEngine/RuleEngine.java
+++ src/main/java/com/nhnacademy/RuleEngine/RuleEngine.java
@@ -19,11 +19,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RuleEngine extends ActiveNode implements Input, Output {
     // TODO 데이터 베이스 클래스 전체를 받아와서 처리 하자! 어때 즐겁지?
-    // private DB database;
+    private InMemoryRepo repo;
     private Set<Wire> outWires = new HashSet<>();
     private Set<Wire> inWires = new HashSet<>();
 
-    protected RuleEngine(String name) {
+    /* protected RuleEngine(String name) {
+        super(name);
+    } */
+    protected RuleEngine(String name, InMemoryRepo repo) {
+        this.repo = repo;
         super(name);
     }
 
@@ -47,17 +51,24 @@ public class RuleEngine extends ActiveNode implements Input, Output {
                     JSONObject content = ((JsonMessage) msg).getContent();
                     switch (TypeExecute(content)) {
                     case 11:
-                        if (MqttToDB()) {
+                        if (SendMqttAndDB(content) != null) {
                             log.info("Success input DB from Mqtt");
                         } else {
                             throw new FailedInputException();
                         }
                         break;
                     case 12:
-                        DBToMqtt();
+                        JsonMessage outMqttMessage = DBToMqtt(content);
+                        if(outMqttMessage != null){
+                            Wire outWire = new Wire();
+                            outWire.getMessageQue().add(outMqttMessage);
+                            wireOut(outWire);
+                        }else{
+                            throw new FailedOutputException();
+                        }
                         break;
                     case 21:
-                        if (ModbusToDB()) {
+                        if (ModbusToDB(content)) {
                             log.info("Success input DB from Modbus");
                         } else {
                             throw new FailedInputException();
@@ -65,7 +76,14 @@ public class RuleEngine extends ActiveNode implements Input, Output {
                         
                         break;
                     case 22:
-                        DBToModbus();
+                        JsonMessage outModbusMessage = DBToModbus(content);
+                        if(outModbusMessage != null){
+                            Wire outWire = new Wire();
+                            outWire.getMessageQue().add(outModbusMessage);
+                            wireOut(outWire);
+                        }else{
+                            throw new FailedOutputException();
+                        }
                         break;
 
                     default:
@@ -91,24 +109,60 @@ public class RuleEngine extends ActiveNode implements Input, Output {
         }
     }
 
-    public boolean MqttToDB() {
-        // TODO db의 데이터 타입과 같은 타입으로 리턴 해서 넣을 것;
-        return false;
+    public JsonMessage SendMqttAndDB(JSONObject content) {
+        String Euid = content.getString("devEui");
+        String sensor = content.getString("sensor");
+        int value = content.getInt("value");
+        try {
+            repo.setDb(Euid,sensor,value);
+            JsonMessage sendJsonMessage = new JsonMessage(content);
+            return sendJsonMessage;
+        } catch (Exception e) {
+            log.warn("Do not have any keys");
+            return null;
+        }
     }
 
-    public JsonMessage DBToMqtt() {
-        // TODO db의 데이터 타입을 형식에 맞춰서 보내줄 것;
-        return null;
+    public JsonMessage DBToMqtt(JSONObject content) {
+        String Euid = content.getString("devEui");
+        String sensor = content.getString("sensor");
+        int value;
+        try {
+            value = repo.getDb(Euid,sensor);
+            content.put("value", value);
+            JsonMessage sendMessage = new JsonMessage(content);
+            return sendMessage;
+        } catch (Exception e) {
+            log.warn("Do not have any sensor");
+            return null;
+        }
+        
     }
 
-    public boolean ModbusToDB() {
-        // TODO db의 데이터 타입과 같은 타입으로 리턴 해서 넣을 것;
-        return false;
+    public boolean ModbusToDB(JSONObject content) {
+        int registerID = content.getInt("adress");
+        int value = content.getInt("value");
+        try{
+            repo.setDb(registerID,value);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
     }
 
-    public JsonMessage DBToModbus() {
-        // TODO db의 데이터 타입을 형식에 맞춰서 보내줄 것;
-        return null;
+    public JsonMessage DBToModbus(JSONObject content) {
+        int registerID = content.getInt("adress");
+        int value;
+        try {
+            value = repo.getDb(registerID);
+            content.put("value", value);
+            JsonMessage sendJsonMessage = new JsonMessage(content);
+            return sendJsonMessage;
+        } catch (Exception e) {
+            log.warn("Do not have registerId!");
+            return null;
+        }
+        
     }
 
 }
