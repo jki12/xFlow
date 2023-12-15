d5f608086255e2f0137277abfd13a7215037140f
haedanui <rhs9705kr@naver.com>
2023-12-15 13:40:59 +0900

    fix : 풀 리퀘스트 코멘트 반영

--- src/main/java/com/nhnacademy/databaserepo/InMemoryRepo.java
+++ src/main/java/com/nhnacademy/databaserepo/InMemoryRepo.java
@@ -16,20 +16,18 @@ import lombok.extern.slf4j.Slf4j;
  * InMemoryRepo
  * <p>
  * Constructor를 통해 Repo를 초기화 할 때 미리 만들어 둔 json 파일을 불러와 static 제어자를 달아 어떤 클래스에서
- * 불러와도 같은 값을 가지는 Map 파일을 불러온다.
+ * 불러와도 같은 값을 가지는 Map 파일을 초기화한다.
  * <p>
- * Map 쓰는 이유는 put을 통해 같은 키를 가지는 값이 들어오면 새로 들어온 값으로 덮어씌우기
- * 때문이다.
- * 
+ * Map 쓰는 이유는 put을 통해 같은 키를 가지는 값이 들어오면 새로 들어온 값으로 덮어씌우기 때문이다.
  * <p>
- * 전체적으로 하는 일은 json 파일을 불러와 Map에 추가한 후
- * 레지스터 주소를 키 값으로,
+ * 전체적으로 하는 일은 json 파일을 불러와 Map에 추가한 후 레지스터 주소를 키 값으로,
  * devEui, 센서이름, 타입, 값, TransactionID, UnitID를 value로
  * 저장해 두고 다른 노드들에서 데이터를 요청하는 경우 키 값을 요청하면
  * 키 값에 해당하는 key, value값을 JSONObject로 변환하여 리턴해 준다.
  */
 @Slf4j
 public class InMemoryRepo implements Repo {
+
     private static Map<String, Object> database = new HashMap<>();
     private JSONObject jsonObject;
 
@@ -57,15 +55,15 @@ public class InMemoryRepo implements Repo {
     }
 
     /**
-     * key값을 받아와 database의 key값에 해당하는 value값을 리턴하는 함수이다.
+     * key값을 받아와 database의 key값에 해당하는 JSONObject값을 리턴하는 함수이다.
      * 
      * @param key key값을 받아온다.
-     * @return database의 key값에 해당하는 value값을 리턴한다.
-     * @throws NullPointerException database에서 해당하는 key값을 찾을 수 없는 경우 해당 Exception을
-     *                              던진다.
+     * @return database의 key값에 해당하는 value값인 JSONObject를 리턴한다.
+     * @throws NullPointerException database에서 해당하는 key값을 찾을 수 없는 경우
+     *                              NullPointerException을 던진다.
      */
-    public String getMapValues(String key) throws NullPointerException {
-        return database.get(key).toString();
+    public JSONObject getMapValues(String key) throws NullPointerException {
+        return new JSONObject(database.get(key));
     }
 
     /**
@@ -99,15 +97,20 @@ public class InMemoryRepo implements Repo {
      * 새로운 값으로 바꿔준다.
      * <p>
      * 이거는 JSONObject 형식이 완전히 지켜져서 들어와야 하기 때문에 효율성이 좋지 않다.
-     * 
+     * @deprecated 사용하지 않을거 같은 함수
      * @param jsonObject
      */
+    @Deprecated
     public void setDBValue(JSONObject jsonObject) {
         Iterator<String> keys = jsonObject.keys();
         while (keys.hasNext()) {
             String key = keys.next();
             database.put(key, jsonObject.get(key));
         }
+    }
+
+    public boolean equalsTest(String key, String devEui, String sensorType){
+        return getDataValue(key, "devEui").equals(devEui) && getDataValue(key, "sensorType").equals(sensorType);
     }
 
     /**
@@ -121,9 +124,8 @@ public class InMemoryRepo implements Repo {
         Iterator<String> keys = database.keySet().iterator();
         while (keys.hasNext()) {
             String key = keys.next();
-            if (getDataValue(key, "devEui").equals(devEui)
-                    && getDataValue(key, "sensorType").equals(sensorType)) {
-                JSONObject json = new JSONObject(getMapValues(key));
+            if (equalsTest(key, devEui, sensorType)) {
+                JSONObject json = getMapValues(key);
                 String s = String.valueOf(json.get("value"));
                 return Double.parseDouble(s);
             }
@@ -145,14 +147,13 @@ public class InMemoryRepo implements Repo {
      * @param value      바꾸고 싶은 value값을 받아온다.
      * @return 값을 바꾸는데 성공하면 True, 키 값이 없거나 바꾸는데 실패시 False를 반환한다.
      */
-    public boolean setDBValue(String devEui, String sensorType, long value) {
+    public boolean setDBValue(String devEui, String sensorType, double value) {
         Iterator<String> keys = database.keySet().iterator();
         while (keys.hasNext()) {
             String key = keys.next();
-            if (getDataValue(key, "devEui").equals(devEui)
-                    && getDataValue(key, "sensorType").equals(sensorType)) {
+            if (equalsTest(key, devEui, sensorType)) {
                 JSONObject outJson = new JSONObject();
-                JSONObject inJson = new JSONObject(getMapValues(key));
+                JSONObject inJson = getMapValues(key);
                 inJson.put("value", value);
                 outJson.put(key, inJson);
                 database.put(key, outJson.get(key));
@@ -163,15 +164,16 @@ public class InMemoryRepo implements Repo {
     }
 
     /**
-     * db에 있는 key값에 들어있는 값에 value 파라메터 안에 있는 속성의 값을 출력해주는 함수이다.
+     * db에 있는 key값에 들어있는 값에 value JSONObject의 속성값을 찾아 해당하는 속성값의 내용을 반환한다.
      * 
      * @param key          키 값을 받아온다
      * @param valueKeyName 원하는 값의 이름을 적는다.
      * @return 원하는 이름의 값을 반환한다.
      */
+
     public String getDataValue(String key, String valueKeyName) {
         if (database.get(key) != null) {
-            JSONObject json = new JSONObject(getMapValues(key));
+            JSONObject json = getMapValues(key);
             return String.valueOf(json.get(valueKeyName));
         }
 
@@ -185,8 +187,8 @@ public class InMemoryRepo implements Repo {
         // System.out.println(databaseNodeJson.jsonObject);
         // System.out.println(database);
         // System.out.println(inMemoryRepo.getJsonValues("101"));
-        System.out.println(inMemoryRepo.getDataValue("101", "devEui"));
-        System.out.println(inMemoryRepo.getDataValue("101", "sensorType"));
+        // System.out.println(inMemoryRepo.getDataValue("101", "devEui"));
+        // System.out.println(inMemoryRepo.getDataValue("101", "sensorType"));
         // System.out.println(inMemoryRepo.getJsonValues("101"));
         // System.out.println(database.get("101"));
         // inMemoryRepo.setDBValue("24e124128c067999", "temperature", 25);
