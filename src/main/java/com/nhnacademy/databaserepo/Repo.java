7e478bedc4983a4c7285e6ee3c35bcd9b9cf9490
haedanui <rhs9705kr@naver.com>
2023-12-15 14:37:36 +0900

    add : Repo의 필수 함수들 정의

--- src/main/java/com/nhnacademy/databaserepo/Repo.java
+++ src/main/java/com/nhnacademy/databaserepo/Repo.java
@@ -1,5 +1,13 @@
 package com.nhnacademy.databaserepo;
 
+import org.json.JSONObject;
+
 public interface Repo {
-    
+    public boolean setData(String devEui, String sensorType, double value);
+
+    public boolean setData(String registerId, double value);
+
+    public JSONObject getData(String devEui, String sensorType);
+
+    public JSONObject getData(String registerId);
 }
