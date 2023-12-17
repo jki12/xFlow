adebcbc11fe4ec9efc30988bf62415dfd507973a
jki12 <sjak0227@naver.com>
2023-12-17 23:10:39 +0900

    [feat] modbus master

--- src/main/java/com/nhnacademy/node/ModbusMasterNode.java
+++ src/main/java/com/nhnacademy/node/ModbusMasterNode.java
@@ -2,33 +2,106 @@ package com.nhnacademy.node;
 
 import com.nhnacademy.Output;
 import com.nhnacademy.Wire;
+import com.nhnacademy.info.Info;
+import com.nhnacademy.message.JsonMessage;
+import com.nhnacademy.message.Message;
+import com.nhnacademy.util.Util;
 
-import ch.qos.logback.classic.pattern.Util;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@Slf4j
+@Getter
 public class ModbusMasterNode extends ActiveNode implements Output {
-    private static final int MODBUS_TCP = 0x0000;
+    private static final int DEFUALT_INTERVAL = 1_000;
+    private static final int DEFAULT_BUFFER_SIZE = 1024;
+    private static final short DEFAULT_ADDRESS = 100;
+    private static final short DEFAULT_QUANTITY = 2;
+    private static final byte DEFAULT_FUNCTION_CODE = 4;
 
-    private int address;
-    private int quantity;
+    private final Set<Wire> outWires = new HashSet<>();
+    private final Info info = new Info();
+    private String host;
+    private int port;
+    private int transactionId;
+    private byte[] address;
+    private byte[] quantity;
+    private byte functionCode;
 
-    
-
-
-    public ModbusMasterNode(String name) {
-        super(name);
+    public ModbusMasterNode(String name, String host, int port) {
+       this(name, host, port, DEFAULT_FUNCTION_CODE, DEFAULT_ADDRESS, DEFAULT_QUANTITY);
     }
 
-    @Override
-    public void process() {
+    public ModbusMasterNode(String name, String host, int port, byte functionCode, short address, short quantity) {
+        super(name);
 
+        this.host = host;
+        this.port = port;
+
+        this.address = Util.toByteArray(address);
+        this.quantity = Util.toByteArray(quantity);
+        this.functionCode = functionCode;
+    }
+
+    /*
+     * 특정 시간마다 계속 slave에게 값을 요청하는 thread를 실행시킨다.
+     */
+    @Override
+    public void preprocess() {
+        Thread thread = new Thread(() -> {
+            Socket client = null;
+
+            try {
+                client = new Socket(host, port);
+
+            } catch (Exception ignore) {
+                log.error(ignore.getMessage());
+
+                Thread.interrupted();
+            }
+
+            while (!Thread.interrupted()) {
+                try {
+                    // header 부분
+                    byte[] pdu = new byte[] { functionCode, address[0], address[1], quantity[0], quantity[1] };
+                    byte[] mbap = Util.makeMBAP((short) (++transactionId % Short.MAX_VALUE), (short) (pdu.length + 1), (byte) 1);
+                    
+                    client.getOutputStream().write(Util.concat(mbap, pdu));
+                    client.getOutputStream().flush();
+                    
+                    byte[] bytes = new byte[DEFAULT_BUFFER_SIZE];
+                    int len = client.getInputStream().read(bytes);
+
+                    info.increaseReceiveCount();
+                    log.debug("{}", Arrays.toString(Arrays.copyOfRange(bytes, 0, len)));
+
+                    Message msg = new JsonMessage(Util.toJson(bytes, len));
+                    for (var wire : outWires) {
+                        info.increaseSendCount();
+
+                        wire.getMessageQue().add(msg);
+                    }
+
+                    Thread.sleep(DEFUALT_INTERVAL);
+                    
+                } catch (Exception e) {
+                    info.increaseAbnormalCount();
+
+                    log.warn(e.getMessage());
+                }
+            }
+        });
+
+        thread.start();
     }
 
     @Override
     public void wireOut(Wire wire) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'wireOut'");
-    }
-
-    
-    
+        outWires.add(wire);
+    }    
 }
