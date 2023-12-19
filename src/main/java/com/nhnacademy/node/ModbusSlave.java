c4464238d7fe03a842a68b24f3d2f5f5c2bcc9ca
badgelatte <saucy5939@gmail.com>
2023-12-19 22:09:55 +0900

    쓸데 없는 거 삭제, header와 response 만들때 return 하도록 수정, exception 처리 추가

--- src/main/java/com/nhnacademy/node/ModbusSlave.java
+++ src/main/java/com/nhnacademy/node/ModbusSlave.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Base64;
@@ -14,11 +15,13 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 import org.json.JSONObject;
 
 import com.nhnacademy.Input;
 import com.nhnacademy.Wire;
+import com.nhnacademy.exception.Exception02;
 import com.nhnacademy.exception.Exception03;
 import com.nhnacademy.exception.Exception1;
 import com.nhnacademy.exception.Exception4;
@@ -38,8 +41,11 @@ public class ModbusSlave extends ActiveNode implements Input {
 
     private int requestLength;
     private byte functionCode;
+    private byte[] header;
+    private byte[] responsePdu;
     private byte[] request;
     private byte[] response;
+    private byte[] errorBytes;
 
     public ModbusSlave() {
         this("modbus slave");
@@ -60,9 +66,8 @@ public class ModbusSlave extends ActiveNode implements Input {
     }
 
     
-    public void makeResponseHeader(byte[] request) {
-        byte[] header = new byte[7];
-        response = new byte[1024];
+    public byte[] makeResponseHeader(byte[] request) {
+        header = new byte[7];
         // trasactionId - 2/2/2/1/1/
         header[0] = request[0];
         header[1] = request[1];
@@ -79,7 +84,8 @@ public class ModbusSlave extends ActiveNode implements Input {
         header[5] = quantities[1];
         header[6] = request[6];
 
-        System.arraycopy(header, 0, response, 0, header.length);
+        return header;
+        // System.arraycopy(header, 0, response, 0, header.length);
     }
 
     /**
@@ -96,121 +102,16 @@ public class ModbusSlave extends ActiveNode implements Input {
         }
     }
 
-
-    /**
-     * Map에서 key를 통해 저장된 value를 찾고자 한다
-     * @param key value를 찾기 위한 key
-     * @return 찾고자하는 value
-     * @throws NullPointerException map에 key가 없을 경우
-     * @hidden
-     */
-    public String getMapValues(Integer key) throws NullPointerException {
-        return database.get(key);
-    }
-
-
-    
-    // -----------------------------------------------------------------------------------------------
-
-    
-    
-    // public static void main(String[] args) {
-        //     /* byte a = 7;
-    //     System.out.println(a);
-    
-    //     byte b= 1;
-    
-    //     int c = (((b & 0xFF) << 8) | (a & 0xFF));
-    //     System.out.println((b & 0xFF) << 8);
-    //     System.out.println(c); */
-    
-    //     String a = "34";
-    //     byte [] d = Base64.getDecoder().decode(a);
-    //     for (byte b : d) {
-        //         System.out.println(b);
-        
-    //     }
-    // }
-    
-    // endcoding decoding 알아보려고 한 거
-    public static void main3(String[] args) {
-        String df = "25";
-        JSONObject dfkj = new JSONObject(23);
-        byte[] str = {(byte)25};
-        String encodedStr = Base64.getEncoder().encodeToString(df.getBytes());
-        String encodedStr2 = Base64.getEncoder().encodeToString(str);
-        // String encodedJSONOBject = Base64.getEncoder().encodeToString(dfkj.get(23))
-        
-        
-        System.out.println("encoded string: " + encodedStr2);
-        
-        // String encodedStr = "dGVzdA==";
-        byte[] decodedBytes = Base64.getDecoder().decode(encodedStr);
-        byte[] decodedBytes2 = Base64.getDecoder().decode(encodedStr2);
-        
-        System.out.println("decodedBytes" + decodedBytes.length);
-        for (byte b : decodedBytes) {
-            System.out.print(b +"/");
-        }
-        System.out.println("\n");
-        
-        System.out.println("decodedBytes2" + decodedBytes2.length);
-        for (byte b : decodedBytes2) {
-            System.out.print(b +"/");
-        }
-        
-        System.out.println("\n");
-        String decodedStr = new String(decodedBytes);
-        String decodedStr2 = new String(decodedBytes2, StandardCharsets.UTF_8);
-        
-        System.out.println("decoded string: " + decodedStr);
-        System.out.println("decoded string version byte[] : " + decodedStr2);
-        
-        // byte a = 3;
-        // int b = a;
-        
-        // System.out.println((byte)(b*2));
-    }
-    
     /**
      * database 속에 저장된 data 찾아오기(encoding한 상태이므로 decoding하여 돌려준다)
      * @param address
      * @return
      */
     public byte[] getData(int address) {
-        byte[] decodedValue= Base64.getDecoder().decode(getMapValues(address));     
+        byte[] decodedValue= Base64.getDecoder().decode(database.get(address));     
         return decodedValue;
     }
     
-    /* 1. encoding 상태로 저장하기  -> 꺼낼 때 decoding하고 new String하고 다시 byte[]로 
-    2. decoding한 상태로 byte[]로 저장하기 -> 
-    */
-    /**
-     * functionCode 3, 4일 경우 response의 pdu 만들기
-     * @param pdu request의 pdu
-     */
-    public void makeReadRegistersPdu(byte[] pdu){ 
-        int address = ((pdu[1] & 0xFF) << 8) | (pdu[2] & 0xFF);
-        int quantity = pdu[4];
-        
-        byte[] data = new byte[256];    // pdu의 최대 크기를 계산해보니 256이면 충분함
-        byte[] values;
-        for (int i = 0; i < quantity*2; i++) {  //address - 101, quantity-4 -> 101+i(=0) = 101 / 101+i(=1) = 102 / 101+2 = 103 / 101 + 3 = 104
-            if ((values = getData(address+i)).length < 2){      // Data가 byte일 경우
-                data[i*2] = 0;                // response[9+i*2] = 0;
-                data[i*2+1] = values[0];      // response[9+i*2+1] = values[0];
-            }
-            else {                                              // Data가 byte[]일 경우
-                data[i*2] = values[0];
-                data[i*2+1] = values[1];
-            }
-        }
-        
-        response[7] = functionCode;
-        response[8] = (byte)(2 * quantity);    // Byte Count
-        System.arraycopy(data, 0, response, 9, data.length);    // value
-    }
-
     /**
      * database에 value 값 넣기
      * @param address
@@ -219,39 +120,82 @@ public class ModbusSlave extends ActiveNode implements Input {
     public void setData(int address, String value) {
         database.put(address, value);
     }
+
+    /**
+     * functionCode 3, 4일 경우 response의 pdu 만들기
+     * @param pdu request의 pdu
+     */
+    public byte[] makeReadRegistersPdu(byte[] pdu){ 
+        responsePdu = new byte[1024];
+        int address = ((pdu[1] & 0xFF) << 8) | (pdu[2] & 0xFF);
+        int quantity = pdu[4];
+        
+        byte[] data = new byte[256];    // pdu의 최대 크기를 계산해보니 256이면 충분함
+        byte[] values;
+        for (int i = 0; i < quantity*2; i++) {  //address - 101, quantity-4 -> 101+i(=0) = 101 / 101+i(=1) = 102 / 101+2 = 103 / 101 + 3 = 104
+            if ((values = getData(address)) == null) {
+                errorBytes = Exception02.Exception02Error(functionCode);
+                data[i*2] = 0;
+                data[i*2+1] = 0;
+            }else {
+                if (getData(address+i).length < 2){      // Data가 byte일 경우
+                    data[i*2] = 0;                // response[9+i*2] = 0;
+                    data[i*2+1] = values[0];      // response[9+i*2+1] = values[0];
+                }
+                else {                                              // Data가 byte[]일 경우
+                    data[i*2] = values[0];
+                    data[i*2+1] = values[1];
+                }
+            }
+            
+        }
+
+        responsePdu[0] = functionCode;
+        responsePdu[1] = (byte)(2 * quantity);    // Byte Count
+        System.arraycopy(data, 0, responsePdu, 2, data.length);    // value
+        return responsePdu;
+    }
+
     
     /**
      * functionCode가 6일때의 response 만들기
      * @param requestPdu
      */
-    public void makeWriteSingleRegister(byte[] requestPdu) {
-        response[7] = functionCode;
-        int address = ((requestPdu[1] & 0xFF) << 8) | (requestPdu[2] & 0xFF);
-        String value = String.valueOf(((requestPdu[3] & 0xFF) << 8) | (requestPdu[4] & 0xFF));
+    public byte[] makeWriteSingleRegister(byte[] pdu) {
+        responsePdu = new byte[1024];
+
+        int address = ((pdu[1] & 0xFF) << 8) | (pdu[2] & 0xFF);
+        byte [] valuesByte = {pdu[3], pdu[4]};
+        String value = Base64.getEncoder().encodeToString(valuesByte);
         setData(address, value);
-        for (int i = 1; i < requestPdu.length; i++) {
-            response[i+7] = requestPdu[i];
+
+        for (int i = 0; i < pdu.length; i++) {
+            responsePdu[i] = pdu[i];
         }
+
+        return responsePdu;
     }
     
     /**
      * functionCode가 16일때의 response 만들기
      * @param pdu
      */
-    public void makeWriteMultipleRegisters(byte[] pdu) {
+    public byte[] makeWriteMultipleRegisters(byte[] pdu) {
         // functionCode - 1, address - 2, quantity - 2, byteCount - 1, value - 2* N
-        response[7] = functionCode;
+        responsePdu = new byte[1024];
+
         int address = ((pdu[1] & 0xFF) << 8) | (pdu[2] & 0xFF);
         int quantity = ((pdu[3] & 0xFF) << 8) | (pdu[4] & 0xFF);
         for (int i = 0; i < quantity; i++) {
-            String value = String.valueOf(((pdu[i+6] & 0xFF) << 8) | (pdu[i+7] & 0xFF));
-            setData(address+i, value);
+            byte[] valuesByte = {pdu[i+6], pdu[i+7]};
+            String value = Base64.getEncoder().encodeToString(valuesByte);
+            setData(address+i*2, value);
         }
         
-        for (int i = 1; i <= 4; i++) {
-            response[7 + i] = pdu[i];
+        for (int i = 0; i < 5; i++) {
+            responsePdu[i] = pdu[i];
         }
-        
+        return responsePdu;
     }
     
     @Override
@@ -277,38 +221,99 @@ public class ModbusSlave extends ActiveNode implements Input {
                 if ((requestLength = inputStream.read(request)) != -1){   // inputStream에 들어온게 request에 들어간다
                     // if((request.length > 7)&&(6 + request[5]) == requestLength) {
                     // }
-                    // pdu 빠로 뺌
+                    
+                    // pdu 따로 뺌
                     byte[] pdu = new byte[1024];
                     System.arraycopy(request, 7, pdu, 0,  request.length-7);
                     functionCode = pdu[0];
-                    
+                    ByteBuffer byteBuffer;
                     switch (functionCode) {
                         case 3:
                         case 4:
-                        if(0 <= request[11] && request[11] <= 0x7D) {
-                            makeResponseHeader(request);
-                            makeReadRegistersPdu(pdu);
-                        }
-                        else {
-                            Exception03.exception03Error(functionCode);
-                        }
+                            // quantity 범위 벗어났을 경우
+                            if(!(0 <= request[11] && request[11] <= 0x7D)) {
+                                errorBytes = Exception03.exception03Error(functionCode);
+                            }
+                            byte[] requestHeader = makeResponseHeader(request);
+                            byte[] requestPdu = makeReadRegistersPdu(pdu);
+                            
+                            byteBuffer = ByteBuffer.allocate(requestHeader.length+requestPdu.length);
+                            byteBuffer.put(requestHeader);
+                            byteBuffer.put(requestPdu);
+
+                            response = byteBuffer.array();
+                            if(errorBytes != null) {
+                                byteBuffer = ByteBuffer.allocate(requestHeader.length+errorBytes.length+requestPdu.length);
+                                byteBuffer.put(requestHeader);
+                                byteBuffer.put(errorBytes);
+                                byteBuffer.put(requestPdu);
+
+                                response = byteBuffer.array();
+                            }
                         
                         break;
                         
                         case 6:
-                        makeResponseHeader(request);
-                        makeWriteSingleRegister(pdu);
+                        // 비트 연산자(&) 우선 순위 : 8위, 비트 시프트 연산자(<<) :  5위, 비트 OR 연산자(|) : 10위,
+                        // 관계 연산자(<=, >=) 우선 순위 : 6위, 논리 AND 연산자(&&) 우선 순위 11위
+                            // 0x0000 <= Register Value <= 0xFFFF 아니면 exception 3
+                            int registerValue = ((pdu[4] & 0xFF) << 8) | (pdu[5] & 0xFF);
+                            if(!(0 <= registerValue && registerValue <= 0xFFFF)){
+                                errorBytes = Exception03.exception03Error(functionCode);
+                            }
+
+                            requestHeader = makeResponseHeader(request);
+                            requestPdu = makeWriteSingleRegister(pdu);
+                            
+                            byteBuffer = ByteBuffer.allocate(requestHeader.length+requestPdu.length);
+                            byteBuffer.put(requestHeader);
+                            byteBuffer.put(requestPdu);
+
+                            response = byteBuffer.array();
+                            if(errorBytes != null) {
+                                byteBuffer = ByteBuffer.allocate(requestHeader.length+errorBytes.length+requestPdu.length);
+                                byteBuffer.put(requestHeader);
+                                byteBuffer.put(errorBytes);
+                                byteBuffer.put(requestPdu);
+
+                                response = byteBuffer.array();
+                            }
+                        
                         break;
 
                         case 16:
-                        makeResponseHeader(request);
-                        makeWriteMultipleRegisters(pdu);
+                        int quantityValue = ((pdu[4] & 0xFF) << 8) | (pdu[5] & 0xFF);
+                        // 0x0001 <= Quantity of Registers <= 0x007B AND Byte Count == Quantity of Registers x 2 아니면 exception 3
+                            if(!((0 <= pdu[5] && pdu[5] <= 0xFFFF) && (pdu[6] == quantityValue*2))){
+                                errorBytes = Exception03.exception03Error(functionCode);
+                            }
+
+                            requestHeader = makeResponseHeader(request);
+                            requestPdu = makeWriteMultipleRegisters(pdu);
+
+                            byteBuffer = ByteBuffer.allocate(requestHeader.length+requestPdu.length);
+                            byteBuffer.put(requestHeader);
+                            byteBuffer.put(requestPdu);
+
+                            response = byteBuffer.array();
+                            if(errorBytes != null) {
+                                byteBuffer = ByteBuffer.allocate(requestHeader.length+errorBytes.length+requestPdu.length);
+                                byteBuffer.put(requestHeader);
+                                byteBuffer.put(errorBytes);
+                                byteBuffer.put(requestPdu);
+
+                                response = byteBuffer.array();
+                            }
                         break;
 
                         default:
-                        Exception1.exception01Error(functionCode);
-                        // log.info("Unsupported function code : " + functionCode);
-                        break;
+                            errorBytes = Exception1.exception01Error(functionCode);
+                            // >> ? 이건 rsp 어케 써야하는거야?? => 걍 request에다가 error 코드만 더함
+                            // log.info("Unsupported function code : " + functionCode);
+                            System.arraycopy(request, 0, response, 0, 7);
+                            System.arraycopy(errorBytes, 0, response, 7, errorBytes.length);
+                            System.arraycopy(pdu, 0, response, 7+ errorBytes.length, pdu.length);
+                            break;
                     }
                     outputStream.write(response);
                     outputStream.flush();
